803de1bea9d0a272345d9038a784a4884b527dc4
brianthomas <brian.a.thomas@nasa.gov>
2015-09-23 19:20:08 -0400

    Fix

--- server/settings.py
+++ server/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'hello'
+    'django.contrib.staticfiles'
 )
 
 MIDDLEWARE_CLASSES = (
