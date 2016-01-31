6417ec7bb3e8d8778293e4ad53b0ee13c84e20c3
brianthomas <brian.a.thomas@nasa.gov>
2016-01-31 18:52:39 -0500

    change service logging to INFO

--- generator/service.py
+++ generator/service.py
@@ -16,7 +16,7 @@ import logging
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO) 
-logging.getLogger(__name__).setLevel(logging.DEBUG)
+#logging.getLogger(__name__).setLevel(logging.DEBUG)
 
 # this should reflect both this service and the backing
 # assorted libraries
