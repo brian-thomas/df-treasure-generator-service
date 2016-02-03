b1c36cc975b6f55bd786dcde56a74bdbd8c6d730
brianthomas <brian.a.thomas@nasa.gov>
2016-02-02 23:09:42 -0500

    Fix CORS path issue, needed trailing slash!

--- generator/service.py
+++ generator/service.py
@@ -8,17 +8,18 @@
    @email=galactictaco@gmail.com
 
 '''
+from flask import Flask
 from flask import jsonify, request, render_template, send_from_directory, Flask
 from gdftg.generator import Generator
-import os
 import gdftg
+from flask.ext.cors import CORS, cross_origin
 import logging
-from flask.ext.cors import CORS
 
 LOG = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG) 
+logging.basicConfig(level=logging.INFO)
 logging.getLogger(__name__).setLevel(logging.DEBUG)
-logging.getLogger('flask_cors').level = logging.DEBUG
+logging.getLogger('gdftg').setLevel(logging.INFO)
+#logging.getLogger('flask_cors').level = logging.DEBUG
 
 # this should reflect both this service and the backing
 # assorted libraries
@@ -31,7 +32,6 @@ MAX_ENCHANTMENTS=10
 LIBRARY_VERSION = gdftg.version
 
 app = Flask(__name__, static_folder='static', static_url_path='')
-#CORS(app, resources={r"/v1/*": {'origins': '*', 'allow_headers' : '*'}})
 CORS(app)
 
 def _abort(code, msg):
@@ -92,12 +92,6 @@ def __generate_treasure (ttype="All", number=1, rformat='json', args=None):
                          items = items.as_list()
                        )
 
-
-def options (self):
-    return {'Allow' : 'PUT' }, 200, \
-           { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers' : '*', \
-             'Access-Control-Allow-Methods' : 'PUT,GET' }
-
 # Endpoints/Routes
 
 @app.route('/', methods=['GET'])
@@ -114,24 +108,27 @@ def send_file(filename):
     LOG.debug("Called static file recall file:"+filename)
     return send_from_directory(app.static_folder, filename) #, mimetype='image/vnd.microsoft.icon')
 
-@app.route('/v1/generate/<int:number>/<ttype>/', methods=['GET', 'OPTIONS'])
-def generate_treasure_full(ttype, number=1):
+@app.route('/v1/generate/<int:number>/<string:ttype>/')
+@app.route('/v1/generate/<int:number>/<string:ttype>')
+def generate_treasure_full (ttype='All', number=1):
     LOG.debug("Called /v1/generate/<num>/<ttype>")
     return __generate_treasure(ttype=ttype, number=number, args=request.args) 
             
-@app.route('/v1/generate/<int:number>/', methods=['GET'])
+@app.route('/v1/generate/<int:number>/')
+@app.route('/v1/generate/<int:number>')
 def generate_treasure_num_only(number):
     LOG.debug("Called /v1/generate/<num>")
     return __generate_treasure(number=number, args=request.args) 
     
-@app.route('/v1/generate/', methods=['GET'])
+@app.route('/v1/generate/')
+@app.route('/v1/generate')
 def generate_treasure_single():
     LOG.debug("Called /v1/generate/")
     return __generate_treasure(args=request.args) 
 
-@app.route('/v1/ttypes/', methods=['GET'])
+@app.route('/v1/ttypes/')
+@app.route('/v1/ttypes')
 def treature_types():
-    LOG.debug("Called /v1/ttypes")
     
     gen = Generator()
     ttypes = sorted(list(gen.treasureClasses()))
